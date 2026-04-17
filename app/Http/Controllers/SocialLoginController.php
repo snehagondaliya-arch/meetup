71a9430c7ffe36e80bb0562cbc73c002c7cf2921
sneha.gondaliya <sneha.gondaliya@grewon.com>
2026-04-17 15:28:24 +0530

    login

--- app/Http/Controllers/SocialLoginController.php
+++ app/Http/Controllers/SocialLoginController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\SocialAccount;
+
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
@@ -11,11 +11,11 @@ use Laravel\Socialite\Facades\Socialite;
 
 class SocialLoginController extends Controller
 {
-    protected $allowedProviders = ['google', 'facebook', 'apple'];
+    protected $allowedProviders = ['google', 'apple'];
 
     public function redirect($provider)
     {
-        if (! in_array($provider, $this->allowedProviders)) {
+        if (!in_array($provider, $this->allowedProviders)) {
             abort(403);
         }
 
@@ -24,34 +24,20 @@ class SocialLoginController extends Controller
 
     public function callback($provider)
     {
-        if (! in_array($provider, $this->allowedProviders)) {
+        if (!in_array($provider, $this->allowedProviders)) {
             abort(404);
         }
         $socialUser = Socialite::driver($provider)->stateless()->user();
 
-        $socialAccount = SocialAccount::where('provider', $provider)
-            ->where('provider_id', $socialUser->getId())
-            ->first();
-
-        if ($socialAccount) {
-            Auth::login($socialAccount->user);
-            return response()->json(['sucees' => 'user registered successfully']);
-        }
-
-        $user = User::where('email', $socialUser->getEmail())->first();
-
-        if (!$user) {
-            $user = User::create([
-                'name' => $socialUser->getName() ?? 'User',
-                'email' => $socialUser->getEmail(),
-                'password' => bcrypt(Str::random(16)), 
-            ]);
-        }
-        $user->socialAccounts()->create([
-                'user_id' => $user->id,
-                'provider' => $provider,
-                'provider_id' => $socialUser->getId(),
+        $user = User::create([
+            'name' => $socialUser->getName() ?? 'User',
+            'email' => $socialUser->getEmail(),
+            'password' => bcrypt(Str::random(16)),
+            'provider' => $provider,
+            'provider_id' => $socialUser->getId(),
+            'profile' => $socialUser->getAvatar(),
         ]);
+
 
         Auth::login($user);
 
