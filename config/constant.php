1f68e60798bbfd48dbbfed25fceb8c1d2b118761
sneha.gondaliya <sneha.gondaliya@grewon.com>
2026-04-20 17:01:40 +0530

    plaveholder

--- config/constant.php
+++ config/constant.php
@@ -2,3 +2,6 @@
 if(!defined('ASSETS_VERSION')) {
     define('ASSETS_VERSION', '?v=1.0.0');
 }
+if(!defined('PLACEHOLDER_IMAGE')) {
+    define('PLACEHOLDER_IMAGE', 'uploads/images/placeholder.jpg');
+}
