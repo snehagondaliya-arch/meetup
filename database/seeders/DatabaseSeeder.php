4e40f4ab196ff0760d55e4ed3500c33dbb33ce85
sneha.gondaliya <sneha.gondaliya@grewon.com>
2026-04-28 12:17:38 +0530

    added class in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+
+use Database\Seeders\CategoryIconSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
