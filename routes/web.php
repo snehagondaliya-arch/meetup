863aa404afa84d8613a6939436e84b84b721640c
sneha.gondaliya <sneha.gondaliya@grewon.com>
2026-05-14 15:54:42 +0530

    removed login route

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,3 @@ Route::post('/organization/login', [OrganizationAuthController::class, 'login'])
 Route::middleware('auth:organization')->prefix('organization')->group(function () {
     Route::resource('events', EventDataController::class)->except(['show']);
 });
-
-//     Route::get('/login', function () {
-//         return redirect('/');
-// })->name('login');
